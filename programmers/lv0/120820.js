6cebad4c20e9302224ed4eec1258fae290b27cee
codeeon <yeon.develop@gmail.com>
2023-11-14 22:25:26 +0900

    나이 출력

--- programmers/lv0/120820.js
+++ programmers/lv0/120820.js
@@ -1,9 +1,7 @@
 // 나이 출력
 // https://school.programmers.co.kr/learn/courses/30/lessons/120820
 
-const solution = (age) => (2022 - age) + 1;
-
-
+const solution = (age) => 2022 - age + 1;
 
 // function solution(age) {
 //     return new Date().getFullYear() - age + 1;
