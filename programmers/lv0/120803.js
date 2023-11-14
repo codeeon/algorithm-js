e38433e530f084e6edbf7b1082093d8725e0956d
codeeon <yeon.develop@gmail.com>
2023-11-14 22:20:55 +0900

    두 수의 차

---
+++ programmers/lv0/120803.js
@@ -0,0 +1,8 @@
+// 두 수의 차
+// https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+// function solution(num1, num2) {
+//   return num1 - num2;
+// }
+
+const solution = (num1, num2) => num1 - num2;
