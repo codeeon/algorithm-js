6dc477621e16456cd9a634a85d6557816c6d20f0
codeeon <yeon.develop@gmail.com>
2023-11-21 10:24:36 +0900

    231121

--- test.js
+++ test.js
@@ -1,16 +1,28 @@
 // 배열 메소드 - Array() constructor, fill, map, filter, reduce
-const solution3 = (n) => {
-  return Array(n)
-    .fill()
-    .map((_, i) => i + 1);
-};
+// const solution3 = (n) => {
+//   return Array(n)
+//     .fill()
+//     .map((_, i) => i + 1);
+// };
 
-console.log(Array(10));
-console.log(Array(10).fill());
-console.log(
-  Array(10)
-    .fill()
-    .map((_, i) => i)
-);
+// console.log(Array(10));
+// console.log(Array(10).fill());
+// console.log(
+//   Array(10)
+//     .fill()
+//     .map((_, i) => i)
+// );
 
-console.log(solution3(10));
+// console.log(solution3(10));
+
+console.log(Boolean(0));
+console.log(Boolean(1));
+console.log(Boolean(4));
+
+console.log(16 & 1, 16 & 2, 16 & 3, 16 & 5, 16 & 16)
+// 0, 0, 0, 0, 16
+console.log(15 & 1, 15 & 2, 15 & 3, 15 & 5, 15 & 15, 15 & 16)
+// 1, 2, 3, 5, 15, 0
+
+console.log(0 == false)
+console.log(0 === false)
